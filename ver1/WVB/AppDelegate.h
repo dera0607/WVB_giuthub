8d366643fdafcbbe1a80170dba1a0ba61f4d4b42
dera0607 <yuko.takadera.0607@gmail.com>
2014-01-07 15:45:31 +0800

    ひとまず画像入って動く状態まで戻ったところ

---
+++ ver1/WVB/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  WVB
+//
+//  Created by NexSeed on 13/11/28.
+//  Copyright (c) 2013年 NexSeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) NSString *Pass_NameData;
+@property (strong, nonatomic) UIImage *Pass_NameImage;
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (nonatomic ,assign) int SelectedSection;
+@property (nonatomic ,assign) int SelectedRow;
+
+@end
